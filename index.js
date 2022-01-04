4c6d4f0e5df3cf766d26452c29f25a63716a47b3
Angappan <angappan.ganesan@walmart.com>
2022-01-04 19:58:54 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import format from "date-format";
+import express from "express";
+
+const app = express();
+const PORT = 4000 || process.env.PORT
+
+app.get('/home', (req, res) => {
+    res.send("Hello world this is Home path")
+})
+
+app.get('/get/insta', (req, res) => {
+    const instaData = {
+        users: [{
+            name: "instaUser",
+            followers: 50,
+            follows: 100
+        }],
+        date: Date.now()
+    }
+    return res.status(200).json(instaData)
+})
+
+app.get('/get/facebook', (req, res) => {
+    const facebookData = {
+        users: [{
+            name: "facebookUser",
+            followers: 50,
+            follows: 100
+        }],
+        date: Date.now()
+    }
+    return res.status(200).json(facebookData)
+})
+
+app.get('/get/linkedin', (req, res) => {
+    const linkedInData = {
+        users: [{
+            name: "linkedInUser",
+            followers: 50,
+            follows: 100
+        }],
+        date: Date.now()
+    }
+    return res.status(200).json(linkedInData)
+})
+
+app.get('/get/data/:id', (req, res)=> {
+    return res.status(200).json({data: req.params.id})
+})
+
+app.listen(PORT, () => {
+    console.log(`${format.asString("dd-MM-yyyy hh:mm:ss", new Date())} || Server is up and running in ${PORT}`)
+})
